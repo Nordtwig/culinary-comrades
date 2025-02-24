c05bc1e867f40e57ed0bec3a6d65d35fbdcfb575
Noah <nordqvist.gaming@gmail.com>
2025-02-24 18:24:35 +0100

    set up better movement and jump

--- culinary_comrades/Player/player.gd
+++ culinary_comrades/Player/player.gd
@@ -2,7 +2,13 @@ extends CharacterBody3D
 class_name Player
 
 
-@export var move_speed: float = 5.0
+@export var normal_speed: float = 10.0
+@export var sprint_speed: float = 20.0
+@export var jump_velocity: float = 90.0
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var is_sprinting: bool = false
+var is_grounded: bool = false
 
 
 func _ready() -> void:
@@ -10,5 +16,26 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector3(Input.get_axis("move_left", "move_right"), 0, Input.get_axis("move_forward", "move_backward")).normalized() * move_speed
+	if is_on_floor():
+		is_sprinting = Input.is_action_pressed("sprint")
+	
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = jump_velocity
+	
+		if not is_grounded:
+			is_grounded = true
+	
+	else:
+		velocity.y -= gravity
+	
+		if is_grounded:
+			is_grounded = false
+	
+	var speed := normal_speed if not is_sprinting else sprint_speed
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	velocity.z = direction.z * speed
+	velocity.x = direction.x * speed
+	
 	move_and_slide()
