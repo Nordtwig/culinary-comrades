b64e7eb4135d98dd24dba3b20bcff8c1abacc262
Noah <nordqvist.gaming@gmail.com>
2025-02-24 20:47:22 +0100

    add a little environment and placeable stove

--- culinary_comrades/Player/player.gd
+++ culinary_comrades/Player/player.gd
@@ -2,20 +2,37 @@ extends CharacterBody3D
 class_name Player
 
 
-@export var normal_speed: float = 10.0
-@export var sprint_speed: float = 20.0
-@export var jump_velocity: float = 90.0
+@export var normal_speed: float = 6.0
+@export var sprint_speed: float = 12.0
+@export var jump_velocity: float = 4.0
+@export var mouse_sensitivity: float = 0.005
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var is_sprinting: bool = false
 var is_grounded: bool = false
 
+@onready var camera: Camera3D = $Body/Camera3D
 
 func _ready() -> void:
-	print("player spawned")
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	print(PI/2)
 
 
 func _physics_process(delta: float) -> void:
+	move()
+
+
+func _input(event) -> void:
+	if event is InputEventMouseMotion:
+		look_around(event.relative)
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func  move() -> void:
 	if is_on_floor():
 		is_sprinting = Input.is_action_pressed("sprint")
 	
@@ -39,3 +56,9 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction.x * speed
 	
 	move_and_slide()
+
+
+func look_around(relative:Vector2):
+	rotate_y(-relative.x * mouse_sensitivity)
+	camera.rotate_x(-relative.y * mouse_sensitivity)
+	camera.rotation.x = clampf(camera.rotation.x, -PI/2, PI/2)
